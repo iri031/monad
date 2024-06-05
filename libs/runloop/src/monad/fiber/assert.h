755fe976481ff86656ac8ab5994bc771dfdd37af
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2024-06-05 20:25:59 +0800

    fiber uses core/assert.h

--- libs/runloop/src/monad/fiber/assert.h
+++ libs/runloop/src/monad/fiber/assert.h
@@ -1,27 +1,19 @@
 #pragma once
 
-#include <assert.h>
+#include <monad/core/assert.h>
 #include <stdlib.h>
 
-#if defined(NDEBUG)
-    #define MONAD_ASSERT(expr)                                                 \
-        if (!(expr)) {                                                         \
-            fprintf(                                                           \
-                stderr,                                                        \
-                "%s(%d) [%s] Assertion failed " #expr "\n",                    \
-                __FILE__,                                                      \
-                __LINE__,                                                      \
-                __func__);                                                     \
-            exit(EXIT_FAILURE);                                                \
-        }
-
-#else
-    #define MONAD_ASSERT(Expression) assert(Expression)
-#endif
-#define MONAD_DEBUG_ASSERT(Expression) assert(Expression)
-#define MONAD_CCALL_ASSERT(Expression)                                         \
-    {                                                                          \
-        int _return_value_ = Expression;                                       \
-        (void)_return_value_;                                                  \
-        MONAD_ASSERT(!_return_value_);                                         \
+#define MONAD_CCALL_ASSERT(Expression)     \
+    {                                      \
+        int _return_value_ = Expression;   \
+        (void)_return_value_;              \
+        if (MONAD_LIKELY(!_return_value_)) \
+        {                                  \
+        }                                  \
+        else                               \
+        {                                  \
+            errno = _return_value_;        \
+            perror(#Expression " failed"); \
+        }                                  \
+        MONAD_ASSERT(!_return_value_);     \
     }
