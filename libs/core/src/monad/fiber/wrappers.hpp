28ae8e154bbfad88d726a6f6afcb66470b5f60cc
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2024-06-21 22:25:10 +0800

    added promise/future imitation to wrappers.hpp

--- libs/core/src/monad/fiber/wrappers.hpp
+++ libs/core/src/monad/fiber/wrappers.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
 #include <monad/fiber/channel.h>
+#include <monad/fiber/config.hpp>
 #include <monad/fiber/scheduler.h>
+
+#include <boost/system/result.hpp>
+
+
+#define MONAD_FIBER_THROW_SYSTEM_EC(Code, Message)                           \
+    do {                                                                     \
+        boost::system::error_code _ec_;                                      \
+        constexpr static boost::source_location loc{BOOST_CURRENT_LOCATION}; \
+        _ec_.assign((Code), boost::system::system_category(), &loc);         \
+        boost::throw_exception(boost::system::system_error(_ec_, (Message)), \
+                               BOOST_CURRENT_LOCATION);                      \
+    } while(true)
+
 
 MONAD_FIBER_NAMESPACE_BEGIN
 
@@ -78,6 +92,34 @@ private:
     monad_fiber_channel_t impl_;
 };
 
+template<typename T>
+struct channel<T&>
+{
+    channel(size_t capacity)
+    {
+        monad_fiber_channel_create(&impl_, capacity, sizeof(T));
+    }
+
+    channel(const channel &) = delete;
+
+    ~channel()
+    {
+        monad_fiber_channel_destroy(&impl_);
+    }
+
+
+    void close() { monad_fiber_channel_close(&impl_); }
+
+    bool try_read (      T& value) { return monad_fiber_channel_try_read(&impl_, & value);}
+    bool try_write(const T& value) { return monad_fiber_channel_try_write(impl_, &value);}
+
+    int read (      T* value) { return monad_fiber_channel_read (&impl_, &value);}
+    int write(      T& value) { return monad_fiber_channel_write(&impl_, &value);}
+    int send (      T& value) { return monad_fiber_channel_send (&impl_, &value);}
+
+private:
+    monad_fiber_channel_t impl_;
+};
 
 
 template<>
@@ -107,6 +149,124 @@ struct channel<void>
 
 private:
     monad_fiber_channel_t impl_;
+};
+
+
+// write a shared state
+namespace detail
+{
+
+}
+
+template< typename T >
+struct future
+{
+    future() noexcept = default;
+
+    future( future const& other) = delete;
+
+    future & operator=( future const& other) = delete;
+
+    future( future && other) noexcept = default;
+
+    future & operator=( future && other) noexcept = default;
+
+    ~future() = default;
+
+    bool valid() const noexcept { return !!impl_; };
+
+    inline T get()
+    {
+        T val{};
+
+        auto p = impl_.lock();
+        if (!p)
+            MONAD_FIBER_THROW_SYSTEM_EC(EPIPE, "promise expired");
+
+
+        auto code = p->read(val);
+        if (code)
+            MONAD_FIBER_THROW_SYSTEM_EC(code, "read");
+        else
+            p->close();
+        
+        return val;
+    }
+
+  private:
+    template<typename>
+    friend struct promise;
+
+    std::weak_ptr<channel<T>> impl_;
+};
+
+
+template<>
+inline void future<void>::get()
+{
+    auto p = impl_.lock();
+    if (!p)
+        MONAD_FIBER_THROW_SYSTEM_EC(EPIPE, "promise expired");
+
+    auto code = p->read();
+    if (code)
+        MONAD_FIBER_THROW_SYSTEM_EC(code, "read");
+    else
+        p->close();
+
+
+}
+
+template< typename R >
+struct promise
+{
+    promise() : impl_(std::make_shared<channel<R>>(1u)) {}
+
+    template< typename Allocator >
+    promise( std::allocator_arg_t, Allocator alloc)
+            : impl_(std::allocate_shared<channel<R>>(std::move(alloc), 1u))
+    {}
+
+    promise( promise &&) noexcept = default;
+
+    promise & operator=( promise &&) noexcept = default;
+
+    promise( promise const&) = delete;
+
+    promise & operator=( promise const&) = delete;
+
+    ~promise() = default;
+
+
+    future< R > get_future()
+    {
+        return future< R >{impl_};
+    }
+
+    // void call
+
+    template<typename _ = void,
+             typename = std::enable_if_t<std::is_same_v<_, R>>>
+    void set_value()
+    {
+        auto code = impl_->send();
+        if (code)
+            MONAD_FIBER_THROW_SYSTEM_EC(code, "channel::send");
+    }
+
+    template<typename T,
+              typename = std::enable_if_t<
+                  std::is_same_v<T, R> || !std::is_void_v<R>>>
+    void set_value(T && value)
+    {
+        auto code = impl_->send(std::forward<T>(value));
+        if (code)
+            MONAD_FIBER_THROW_SYSTEM_EC(code, "channel::send");
+    }
+
+  private:
+
+    std::shared_ptr<channel<R>> impl_;
 };
 
 MONAD_FIBER_NAMESPACE_END
