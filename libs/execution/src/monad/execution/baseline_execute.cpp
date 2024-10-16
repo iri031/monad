c650a392c05b51b2c27ffcc278e77292faa2f269
goodlyrottenapple <goodlyrottenapple@gmail.com>
2024-10-16 10:24:38 -0400

    Dump contracts used at least 6 times in the history of mainnet

--- libs/execution/src/monad/execution/baseline_execute.cpp
+++ libs/execution/src/monad/execution/baseline_execute.cpp
@@ -31,7 +31,28 @@
 
 #include <memory>
 
+#include <quill/LogLevel.h>
+#include <quill/Quill.h>
+#include <quill/detail/LogMacros.h>
+
+#include <tbb/concurrent_hash_map.h>
+#include <monad/core/assert.h>
+#include <filesystem>
+#include <monad/core/keccak.hpp>
+#include <monad/core/basic_formatter.hpp>
+#include <monad/core/fmt/address_fmt.hpp>
+
+
+namespace fs = std::filesystem;
+
 MONAD_NAMESPACE_BEGIN
+
+typedef tbb::concurrent_hash_map<evmc::address,int, tbb::tbb_hash_compare<evmc::address>> ContractTable;
+typedef tbb::concurrent_hash_map<evmc::address,int, tbb::tbb_hash_compare<evmc::address>>::accessor Accessor;
+
+ContractTable call_counter;
+const int CONTRACT_CALL_THRESHOLD = 5;
+
 
 evmc::Result baseline_execute(
     evmc_message const &msg, evmc_revision const rev, evmc::Host *const host,
@@ -66,6 +87,56 @@ evmc::Result baseline_execute(
 #ifdef EVMONE_TRACING
     LOG_TRACE_L1("{}", trace_ostream.str());
 #endif
+
+    if (result.status_code == EVMC_SUCCESS) {
+        Accessor accessor;
+        const auto itemIsNew = call_counter.insert( accessor, msg.code_address );
+        if (itemIsNew) {
+            accessor->second = 1;
+        }
+        else {
+            const int call_count = accessor->second;
+            if (call_count != -1) {
+                if (call_count > CONTRACT_CALL_THRESHOLD) {
+                    // mark for insertion into the contracts dump
+                    accessor->second = -1;
+                    auto contracts_dir_var = std::getenv("CONTRACTS_DIR");
+                    MONAD_ASSERT(contracts_dir_var);
+
+                    // content addressable path for the contract using keccak of the actual code
+                    auto const code_hash = keccak256(code_analysis.executable_code);
+
+                    auto code_hash_dir = fs::path(contracts_dir_var) / "code_hash" /
+                            fmt::format("{:02x}", code_hash.bytes[0]) /
+                            fmt::format("{:02x}", code_hash.bytes[1]);
+
+                    fs::create_directories(code_hash_dir);
+
+                    auto contract_path = code_hash_dir / fmt::format("{}", intx::hex(intx::be::load<intx::uint256>(code_hash.bytes)));
+
+                    auto os = std::ofstream(contract_path);
+                    os.write(
+                        reinterpret_cast<char const *>(code_analysis.executable_code.data()),
+                        code_analysis.executable_code.size());
+
+                    auto code_address_dir = fs::path(contracts_dir_var) / "code_address" /
+                            fmt::format("{:02x}", msg.code_address.bytes[0]) /
+                            fmt::format("{:02x}", msg.code_address.bytes[1]);
+
+                    fs::create_directories(code_address_dir);
+
+                    const Address& addr = msg.code_address;
+                    auto contract_address_path = code_address_dir / fmt::format("{}", addr);
+
+                    fs::create_symlink(contract_path, contract_address_path);
+                } 
+                else {
+                    accessor->second = call_count + 1;
+                }
+            }
+        }
+        accessor.release();
+    }
 
     return evmc::Result{result};
 }
