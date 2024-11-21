dbbc3a49d78adb10f30da670b8b4a2770524ab93
Kevin Kuehler <kkuehler@monad.xyz>
2024-11-21 12:45:32 -0500

    append only log: own ifstream

--- libs/execution/src/monad/event/append_only_log_emitter.hpp
+++ libs/execution/src/monad/event/append_only_log_emitter.hpp
@@ -3,18 +3,19 @@
 #include <monad/config.hpp>
 #include <monad/event/event_emitter.hpp>
 
-#include <istream>
+#include <filesystem>
+#include <fstream>
 #include <optional>
 
 MONAD_NAMESPACE_BEGIN
 
 class AppendOnlyLogEmitter : public EventEmitter
 {
-    std::istream &cursor_;
+    std::ifstream cursor_;
 
 public:
     AppendOnlyLogEmitter(
-        std::istream &, monad_execution_event const * = nullptr);
+        std::filesystem::path const &, monad_execution_event const * = nullptr);
 
     std::optional<Event> next_event() override;
 };
