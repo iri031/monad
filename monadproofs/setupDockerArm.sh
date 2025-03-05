285f03e6aa8b7607777782eaa6c050be72679072
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2025-03-05 14:10:40 -0500

    cleanup

--- monadproofs/setupDockerArm.sh
+++
@@ -1,8 +0,0 @@
-set -e
-set +x
-docker pull arm64v8/debian
-docker run --name fvarm -ti -w /root arm64v8/debian bash -l
-docker stop fvarm
-docker cp -a . fvarm:/root/
-docker start fvarm
-docker exec fvarm /root/fv-workspace/setupDebian.sh
