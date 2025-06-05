32e077fb78528ce5ff4da203384ef5cfbde2293a
Alexander Lee <1354409+zander-xyz@users.noreply.github.com>
2025-06-05 15:36:59 -0400

    priority pool map

--- libs/core/src/monad/fiber/priority_pool.hpp
+++ libs/core/src/monad/fiber/priority_pool.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <monad/core/result.hpp>
+#include <monad/fiber/algorithm.hpp>
 #include <monad/fiber/config.hpp>
 #include <monad/fiber/priority_queue.hpp>
 #include <monad/fiber/priority_task.hpp>
 
 #include <boost/fiber/buffered_channel.hpp>
 #include <boost/fiber/condition_variable.hpp>
+#include <boost/fiber/future/promise.hpp>
 #include <boost/fiber/mutex.hpp>
+#include <boost/outcome/try.hpp>
 
+#include <functional>
 #include <future>
+#include <span>
 #include <thread>
 #include <utility>
 
@@ -43,6 +49,30 @@ public:
     void submit(uint64_t const priority, std::function<void()> task)
     {
         channel_.push({priority, std::move(task)});
+    }
+
+    template <class Output1, class F>
+    Result<
+        std::vector<std::remove_reference_t<std::invoke_result_t<F, Output1>>>>
+    submit_and_map(
+        std::span<std::function<Result<Output1>()>> const tasks, F &&f)
+    {
+        // there is a race internal to the promise so the lifetime needs to
+        // extend past this function
+        std::shared_ptr<boost::fibers::promise<Result<Output1>>[]> promises{
+            new boost::fibers::promise<Result<Output1>>[tasks.size()]};
+
+        for (unsigned i = 0; i < tasks.size(); ++i) {
+            submit(i, [i, promises, &task = tasks[i]] {
+                promises[i].set_value(task());
+            });
+        }
+
+        std::vector<std::remove_reference_t<std::invoke_result_t<F, Output1>>>
+            ret{tasks.size()};
+        BOOST_OUTCOME_TRY(fiber::map(
+            std::span{promises.get(), tasks.size()}, f, std::span{ret}));
+        return ret;
     }
 };
 
