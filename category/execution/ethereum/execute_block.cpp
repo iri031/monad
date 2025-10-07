8386a4482fca86da1225062c37702bef647a898b
Daniel Hillerström <dhillerstrom@category.xyz>
2025-10-07 11:59:30 -0400

    [execution] Factor out block {pre,post}process steps

--- category/execution/ethereum/execute_block.cpp
+++ category/execution/ethereum/execute_block.cpp
@@ -307,29 +307,7 @@ Result<std::vector<Receipt>> execute_block(
     MONAD_ASSERT(senders.size() == block.transactions.size());
     MONAD_ASSERT(senders.size() == call_tracers.size());
 
-    {
-        State state{block_state, Incarnation{block.header.number, 0}};
-
-        if constexpr (traits::evm_rev() >= EVMC_PRAGUE) {
-            deploy_block_hash_history_contract(state);
-        }
-
-        set_block_hash_history(state, block.header);
-
-        MONAD_ASSERT(block_state.can_merge(state));
-        block_state.merge(state);
-    }
-
-    if constexpr (traits::evm_rev() >= EVMC_CANCUN) {
-        set_beacon_root(block_state, block.header);
-    }
-
-    if constexpr (traits::evm_rev() == EVMC_HOMESTEAD) {
-        if (MONAD_UNLIKELY(block.header.number == dao::dao_block_number)) {
-            transfer_balance_dao(
-                block_state, Incarnation{block.header.number, 0});
-        }
-    }
+    preprocess_block<traits>(block_state, block.header);
 
     BOOST_OUTCOME_TRY(
         auto const retvals,
@@ -346,6 +324,41 @@ Result<std::vector<Receipt>> execute_block(
             call_tracers,
             revert_transaction));
 
+    postprocess_block<traits>(block_state, block);
+
+    return retvals;
+}
+
+template <Traits traits>
+void preprocess_block(BlockState &block_state, BlockHeader const &header)
+{
+    {
+        State state{block_state, Incarnation{header.number, 0}};
+
+        if constexpr (traits::evm_rev() >= EVMC_PRAGUE) {
+            deploy_block_hash_history_contract(state);
+        }
+
+        set_block_hash_history(state, header);
+
+        MONAD_ASSERT(block_state.can_merge(state));
+        block_state.merge(state);
+    }
+
+    if constexpr (traits::evm_rev() >= EVMC_CANCUN) {
+        set_beacon_root(block_state, header);
+    }
+
+    if constexpr (traits::evm_rev() == EVMC_HOMESTEAD) {
+        if (MONAD_UNLIKELY(header.number == dao::dao_block_number)) {
+            transfer_balance_dao(block_state, Incarnation{header.number, 0});
+        }
+    }
+}
+
+template <Traits traits>
+void postprocess_block(BlockState &block_state, Block const &block)
+{
     State state{
         block_state, Incarnation{block.header.number, Incarnation::LAST_TX}};
 
@@ -361,8 +374,6 @@ Result<std::vector<Receipt>> execute_block(
 
     MONAD_ASSERT(block_state.can_merge(state));
     block_state.merge(state);
-
-    return retvals;
 }
 
 EXPLICIT_TRAITS(execute_block);
